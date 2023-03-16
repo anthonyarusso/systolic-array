6caba195f60b413f2d6d25d6dc283a751103b379
Anthony A Russo <aarusso@ucsc.edu>
2023-03-15 21:18:40 -0700

    Added comments to mac.sv.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ SystemVerilog module for matrix multiplication
 Related resources:
 https://github.com/bespoke-silicon-group/basejump_stl
 http://www.jhauser.us/arithmetic/HardFloat.html
+https://docs.google.com/document/d/1xA5XUzBtz_D6aSyIBQUwFk_kSUdckrfxa2uzGjMgmCU/edit#heading=h.wuakt9cc0y5m
--- systolic_array.srcs/sources_1/new/mac.sv
+++ systolic_array.srcs/sources_1/new/mac.sv
@@ -26,7 +26,33 @@
 // https://github.com/bespoke-silicon-group/basejump_stl/blob/master/bsg_fpu/bsg_fpu_add_sub.v
 // https://github.com/bespoke-silicon-group/basejump_stl/blob/master/bsg_fpu/bsg_fpu_mul.v
 
-module mac(
+module mac
+  #(parameter `BSG_INV_PARAM(e_p)     // exponent width
+    , parameter `BSG_INV_PARAM(m_p)   // mantissa width
+  )
+(
+    input clk_i
+    , input reset_i
+    , input en_i
+    
+    // TODO: Friendly reminder that MAC will need two of each interface...
 
-    );
+    // Consumer interface
+    , input v_i
+    , input [e_p+m_p:0] a_i
+    , input [e_p+m_p:0] b_i
+    , output logic ready_o
+
+    // Producer interface
+    , output logic v_o
+    , output logic [e_p+m_p:0] z_o
+    , output logic unimplemented_o
+    , output logic invalid_o
+    , output logic overflow_o
+    , output logic underflow_o
+    , input yumi_i // when yumi_i is high, en_i also has to be high
+);
+
+// The add_sub and mul modules use all the same IO except add_sub also has a "sub_i" control input which I leave unused.
+
 endmodule
