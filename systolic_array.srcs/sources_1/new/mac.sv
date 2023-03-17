b4df306ee0864e157f99007db249718bfeff8168
Anthony A Russo <aarusso@ucsc.edu>
2023-03-16 19:03:19 -0700

    Added configuration files for the PYNQ-Z1 board and comments to mac.sv.

--- systolic_array.srcs/sources_1/new/mac.sv
+++ systolic_array.srcs/sources_1/new/mac.sv
@@ -37,22 +37,39 @@ module mac
     
     // TODO: Friendly reminder that MAC will need two of each interface...
 
-    // Consumer interface
-    , input v_i
+    // Consumer interface A
+    , input a_v_i
     , input [e_p+m_p:0] a_i
+    , output logic a_ready_o
+    
+    // Consumer interface B
+    , input b_v_i
     , input [e_p+m_p:0] b_i
-    , output logic ready_o
+    , output logic b_ready_o
 
-    // Producer interface
-    , output logic v_o
-    , output logic [e_p+m_p:0] z_o
+    // Producer interface A
+    , output [e_p+m_p:0] a_o
+    , output logic a_v_o
+    , input a_yumi_i // when yumi_i is high, en_i also has to be high
+        
+    // Producer interface B
+    , output [e_p+m_p:0] b_o
+    , output logic b_v_o
+    , input b_yumi_i // when yumi_i is high, en_i also has to be high
+    
+    // Shared producer interface
+    , output logic [e_p+m_p:0] z_o // accumulator output (read after the whole system is finished).
     , output logic unimplemented_o
     , output logic invalid_o
     , output logic overflow_o
     , output logic underflow_o
-    , input yumi_i // when yumi_i is high, en_i also has to be high
 );
 
 // The add_sub and mul modules use all the same IO except add_sub also has a "sub_i" control input which I leave unused.
+
+// If both consumers are ready AND valid, save a_i and b_i to registers and enter the multiplication state.
+// Once the multiplication has finished, add the product to the accumulator register by entering the accumulation state.
+// Once the acclumulation has finished, enter the done state.
+// Once the downstream MACs also enter the done state, re-enter the ready state.
 
 endmodule
