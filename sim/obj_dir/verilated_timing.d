622e24aac078ad79749a093d1db60805954c285c
Anthony A. Russo <aarusso.exe@outlook.com>
2023-03-23 15:43:55 -0700

    Removed obj_dir and other unnecessary files.

--- sim/obj_dir/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
- /home/aarusso/oss-cad-suite/share/verilator/include/verilated_timing.cpp \
- /home/aarusso/oss-cad-suite/share/verilator/include/verilated_timing.h \
- /home/aarusso/oss-cad-suite/share/verilator/include/verilated.h \
- /home/aarusso/oss-cad-suite/share/verilator/include/verilatedos.h \
- /home/aarusso/oss-cad-suite/share/verilator/include/verilated_config.h \
- /home/aarusso/oss-cad-suite/share/verilator/include/verilated_types.h \
- /home/aarusso/oss-cad-suite/share/verilator/include/verilated_funcs.h
