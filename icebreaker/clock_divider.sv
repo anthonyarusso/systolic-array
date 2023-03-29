152c6471ad1e25313f4fe97e097ba3c568e76876
Anthony A. Russo <aarusso.exe@outlook.com>
2023-03-28 23:40:38 -0700

    Added clock_divider.sv

---
+++ icebreaker/clock_divider.sv
@@ -0,0 +1,23 @@
+module
+clock_divider
+#(parameter width_p = 8)
+(input clk_i
+,input en_i
+,input reset_i
+,output slow_clk_o
+);
+
+wire [width_p-1:0] count_w;
+
+counter
+#(.width_p(width_p))
+counter_inst
+(.clk_i(clk_i)
+,.en_i(en_i)
+,.reset_i(reset_i)
+,.count_o(count_w)
+);
+
+assign slow_clk_o = count_w[width_p-1];
+
+endmodule
