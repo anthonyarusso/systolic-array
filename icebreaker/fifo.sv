b3f6b4b6e8f01a214ca8c0de3a082450d4b9561e
Anthony A. Russo <aarusso.exe@outlook.com>
2023-03-30 18:03:41 -0700

    Added comments to fifo.sv.

--- icebreaker/fifo.sv
+++ icebreaker/fifo.sv
@@ -15,18 +15,19 @@ module fifo
 ,output [width_p-1:0] data_o
 );
 
+logic [$clog2(depth_p)-1:0] rd_ptr_r, wr_ptr_r;
 wire [0:0] empty_w, full_w;
 assign empty_w = (rd_ptr_r == wr_ptr_r);
+// WARN: the following approach to full_w only works where depth_p is a power
+// of 2. Otherwise, subtraction of rd_ptr_r-1 won't roll over to depth_p-1.
 assign full_w = (wr_ptr_r == (rd_ptr_r-1));
 assign ready_o = ~full_w;
 assign valid_o = ~empty_w;
     
-logic [$clog2(depth_p)-1:0] rd_ptr_r, wr_ptr_r;
-    
 always_ff @(posedge clk_i) begin
     if (reset_i) begin
         rd_ptr_r <= '0;
-    end else if (yumi_i & ~empty_w) begin
+    end else if (yumi_i && valid_o) begin
         rd_ptr_r <= rd_ptr_r + 1;
     end
 end
@@ -34,7 +35,7 @@ end
 always_ff @(posedge clk_i) begin
     if (reset_i) begin
         wr_ptr_r <= '0;
-    end else if (valid_i & ~full_w) begin
+    end else if (ready_o && valid_i) begin
         wr_ptr_r <= wr_ptr_r + 1;
     end
 end
